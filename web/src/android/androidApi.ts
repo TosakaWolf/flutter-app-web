fa1effc1632b45fe3147fcd7a68ef94c93f7d4e2
Corn <38304261+TosakaWolf@users.noreply.github.com>
2024-07-17 16:44:00 +0800

    Update androidApi.ts

--- web/src/android/androidApi.ts
+++ web/src/android/androidApi.ts
@@ -2,7 +2,7 @@ import { callAndroidMethod } from '../utils/androidRequest'
 import { DefaultApiResult } from './defaultApiResult'
 
 export function showToastBottom(msg: string): void {
-  (window as any).showToastBottom(msg)
+  (window as any).Android.showToastBottom(msg)
 }
 
 export function helloWorld(...args: any[]): Promise<DefaultApiResult> {
