f9af5b1ac247207ec29ce7a155568b40c7f727fd
Corn <38304261+TosakaWolf@users.noreply.github.com>
2024-07-17 09:42:21 +0800

    Update WebAppInterface.java

--- android/app/src/main/java/net/yamamomo/flutter_app_web/WebAppInterface.java
+++ android/app/src/main/java/net/yamamomo/flutter_app_web/WebAppInterface.java
@@ -11,9 +11,9 @@ import android.widget.Toast;
 import net.yamamomo.flutter_app_web.common.ApiResult;
 import net.yamamomo.flutter_app_web.utils.AndroidReplyUtil;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class WebAppInterface {
     Context context;
@@ -28,21 +28,17 @@ public class WebAppInterface {
         this.executorService = Executors.newFixedThreadPool(8); // You can adjust the pool size
     }
 
-
-    // 从页面展示一个Toast,不需要callback
     @JavascriptInterface
     public void showToastBottom(String toastText) {
-        executorService.execute(() -> {
-            Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-            toast.show();
-        });
+        Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
     }
 
     @JavascriptInterface
-    public void helloWorld(final String callbackName, String arg1, String arg2) {
+    public void helloWorld(final String callbackName, String... args) {
         executorService.execute(() -> {
-            AndroidReplyUtil.postApiResult(mainHandler, webView, callbackName, ApiResult.success(true, "hello world 收到参数" + arg1 + "、" + arg2));
+            AndroidReplyUtil.postApiResult(mainHandler, webView, callbackName, ApiResult.success(true, "hello world 收到参数" + Arrays.toString(args)));
         });
     }
 
