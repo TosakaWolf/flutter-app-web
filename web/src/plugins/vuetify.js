ed728cac7119d1ae20f098f9b74a517df54fea46
Corn <38304261+TosakaWolf@users.noreply.github.com>
2024-07-18 09:04:20 +0800

    图标依赖

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,9 +1,14 @@
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
 // import * as directives from 'vuetify/lib/directives/index'
 // import * as components from 'vuetify/lib/components/index'
 import { createVuetify } from 'vuetify'
 
 export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi' // This is already the default value - only for display purposes
+  }
   // components,
   // directives
 })
